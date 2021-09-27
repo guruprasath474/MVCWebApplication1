2bb618f28d2d489c124bf8414c183aec36d4447c
Guru Prasath SR <guruprasath.sr@ivygdcad.ivy-support.com>
2021-09-27 12:41:34 +0530

    Add project files.

---
+++ MVCWebApplication1/Data/MVCMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCWebApplication1.Models;
+
+namespace MVCWebApplication1.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
